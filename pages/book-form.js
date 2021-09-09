dbcaa28bee8b28f3f6187c32f9e51d8f1d62e971
Jose Guerra <jose.guerra@dodsgroup.com>
2021-09-09 10:59:02 +0100

    initial commit

---
+++ pages/book-form.js
@@ -0,0 +1,33 @@
+const bookForm = () => {
+  const formPost = async e => {
+    e.preventDefault()
+
+    const res = await fetch('/api/book', {
+      body: JSON.stringify({
+        title: e.target.title.value,
+        author: e.target.author.value
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      method: 'POST'
+    })
+  
+    const result = await res.json()
+  }
+
+  return (
+    <form onSubmit={formPost}>
+      <label htmlFor="title">Title</label>
+      <input id="title" name="title" type="text" autoComplete="title" required />
+      <br />
+      <label htmlFor="author">Author</label>
+      <input id="author" name="author" type="text" autoComplete="author" required />
+      <br />
+      <button type="submit">Submit</button>
+    </form>
+  )
+}
+
+export default bookForm
